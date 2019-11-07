1183d3b2731035fe385c91ac985dfa09e7333751
Just-a-programmer <manan18294@iiitd.ac.in>
2019-11-07 23:45:21 +0530

    Added Non-restoring Algorithm

---
+++ DivisionAlgorithm.java
@@ -0,0 +1,106 @@
+class DivisionAlgorithm
+{
+	public static int q=0;
+	public static int r=0;
+	private static byte[] registerM = new byte[Main.n];
+	private static byte[] registerQ = new byte[Main.n];//q
+	private static byte[] registerA = new byte[Main.n];//r
+	private static byte[] negative = new byte[Main.n];
+	private static void initialiseRegisters(byte[] dividend, byte[] divisor) 
+	{
+		// Negative 
+		for(byte i=0;i<divisor.length;i++) 
+			negative[i] = divisor[i];
+		Main.optimisedTwosComplement(negative,negative.length);
+		// M
+		Main.clearRegister(registerM);
+		for(byte i=0;i<divisor.length;i++) 
+			registerM[i] = divisor[i];
+		// S
+		Main.clearRegister(registerQ);
+		for(byte i=0;i<dividend.length;i++) 
+			registerQ[i] = dividend[i];
+		//A
+		Main.clearRegister(registerA);
+	}
+	public static boolean isZero(byte[] x)
+	{
+		for(int i=0;i<x.length;i++)
+			if(x[i]==1)
+				return false;
+		return true;
+	}
+	public static void divide(byte[] dividend,byte[] divisor)
+	{
+		int flag=0;
+		int f=0;
+		if(isZero(divisor))
+		{
+			System.out.println("divisor is zero");
+			return;
+		}
+		if(dividend[0]==1)
+		{
+			Main.optimisedTwosComplement(dividend,dividend.length );
+			flag=1;
+		}
+		if(divisor[0]==1)
+		{
+			Main.optimisedTwosComplement(divisor,divisor.length);
+			f=1;
+		}
+		initialiseRegisters(dividend,divisor);
+		int size=Main.n;
+		while(size>0)
+		{
+			byte t=registerA[0];
+			shiftLeft();
+			if(t==1)
+				Main.add(registerA,registerM);
+			else
+				Main.add(registerA,negative);
+			t=registerA[0];
+			if(t==1)
+				registerQ[registerQ.length-1]=0;
+			else
+				registerQ[registerQ.length-1]=1;
+			size--;
+		}
+		if(registerA[0]==1)
+			Main.add(registerA,registerM);
+		r=Main.decimal(registerA,registerA.length);
+		if((flag==1&&f==0)||(flag==0&&f==1))
+			Main.optimisedTwosComplement(registerQ,registerQ.length);
+		if(((registerQ[0]==1&&f==0)||(registerQ[0]==0&&f==1))&&!isZero(registerQ))
+			r=-r;
+		else if(flag==1)
+			r=-r;
+		q=Main.decimal(registerQ,registerQ.length);
+	}
+	public static String getr()
+	{
+		return Main.getString(registerA,registerA.length);
+	}
+	public static String getq()
+	{
+		return Main.getString(registerQ,registerQ.length);
+	}
+	//shift left AQ
+	private static void shiftLeft()
+	{
+		byte a=registerQ[0];
+		for(int i=registerA.length-1;i>=0;i--)
+		{
+			byte temp=registerA[i];
+			registerA[i]=a;
+			a=temp;
+		}
+		a=0;
+		for(int i=registerQ.length-1;i>=0;i--)
+		{
+			byte temp=registerQ[i];
+			registerQ[i]=a;
+			a=temp;
+		}
+	}
+}
--- Main.java
+++ Main.java
@@ -1,85 +1,243 @@
-public class Main {
-    public final static byte n = 12; // size of operands
-    private static byte[] UNITY;
+import java.util.*;
+import java.io.*;
+public class Main 
+{
+	public final static byte n=12; // size of operands
+	private static byte[] UNITY;
+	static 
+	{
+		UNITY=new byte[n];
+		UNITY[n-1] = 1;
+	}
+	//Converts a decimal number into binary and return the binary number in two complement
+	public static byte[] binary(int d) 
+	{
+		int sign = d<0 ? 1 : 0;
+		d = Math.abs(d);
+		byte[] bin = new byte[n];
+		byte i = n;
+		i--;
+		while (d!=0) 
+		{
+			try
+			{
+				bin[i] = (byte) (d%2);
+				d = d/2;
+				i--;
+			}
+			catch(ArrayIndexOutOfBoundsException e)
+			{
+				System.out.println("Number larger than expected");
+				System.exit(0);
+			}	
+		}
+		if (sign==0) 
+			return bin;
+		else 
+			return optimisedTwosComplement(bin,bin.length);
+	}
+	//gives two complement of a number;
+	public static byte[] optimisedTwosComplement(byte [] x,int length)
+	{
+		int i=length-1,flag=0;
+		for(;i>=0;i--)
+			if(x[i]==1)
+				break;
+		i--;
+		for(;i>=0;i--)
+		{
+			if(x[i]==0)
+				x[i]=1;
+			else
+				x[i]=0;
+		}
+		return x;
+	}
+	/*protected static byte[] twosComplement(byte[] bin) 
+	{
+		for(byte i=0;i<Main.n;i++) 
+		{
+			if (bin[i] == 0) 
+				bin[i] = 1;
+			else 
+				bin[i] = 0;
+		}
+		add(bin, Main.UNITY);
+		return bin;
+	} */
+	protected static void add(byte[] x, byte[] y) 
+	{
+		// y is added to x and saved to x
+		// ensure equal size
+		byte carry = 0;
+		for(byte i=1;i<=x.length;i++) 
+		{
+			if (x[x.length-i]==0 && y[x.length-i]==0) 
+			{
+				x[x.length-i] = carry;
+				carry = 0;
+			}
+			else if (x[x.length-i]==1 && y[x.length-i]==1) 
+			{
+				if (carry==1) 
+				{
+					x[x.length-i] = 1;
+					carry = 1;
+				} 
+				else 
+				{
+					x[x.length-i] = 0;
+					carry = 1;
+				}
+			}
+			else 
+			{
+				if (carry==1) 
+				{
+					x[x.length-i] = 0;
+					carry = 1;
+				} 
+				else 
+				{
+					x[x.length-i] = 1;
+				}
+			}
+		}
+	}
+	protected static void shiftRight(byte[] x) 
+	{
+		byte y = x[0];
+		byte temp;
+		for(byte i=1;i<x.length;i++) 
+		{
+			temp = x[i];
+			x[i] = y;
+			y = temp;
+		}
+	}
+	public static int decimal(byte[] x,int length) 
+	{
+		int sum = 0;
+		int flag=0;
+		if(x[0]==1)
+		{
+			optimisedTwosComplement(x,length);
+			flag=1;
+		}
+		// dropping the least significant bit
+		for(int i=length-1;i>=0;i--) 
+		{
+			sum += x[i]*Math.pow(2,length-i-1);
+		}
+		if(flag==1) 
+			return -sum;
+		return sum;
+	}
+	public static void clearRegister(byte[] register) 
+	{
+		for(int i=0;i<register.length;i++) 
+			register[i] = 0;
+	}
+	public static void printArray(byte[] x,int length)
+	{
+		for(int i=0;i<length;i++)
+			System.out.print(x[i]);
+		System.out.println();
+	}
+	public static void test() throws FileNotFoundException
+	{
+		PrintWriter out=new PrintWriter("Sample.txt");
+		try
+		{
+			for(int i=-1000;i<=1000;i++)
+			{
+				for(int j=-1000;j<=1000;j++)
+				{
+					if(j==0)
+						continue;
+					byte[] x2 = binary(i);
+					byte[] y2 = binary(j);
+					BoothsMultiplicationAlgorithm.multiply(x2,y2);
+					DivisionAlgorithm.divide(x2,y2);
+					int a=i/j;
+					int b=i%j;
+					if(i*j!=BoothsMultiplicationAlgorithm.ans||a!=DivisionAlgorithm.q||b!=DivisionAlgorithm.r)
+					{
+						System.out.println(i+" "+j);
+						System.out.println("Q="+a+" "+DivisionAlgorithm.q);
+						System.out.println("R="+b+" "+DivisionAlgorithm.r);
+						System.out.println("Multiplication: "+i*j+" "+BoothsMultiplicationAlgorithm.ans);
+						System.out.println("NO");
+						System.exit(0);
+					}
+					else
+					{
+						String ans="Number 1: "+i+" Number 2: "+j+"\n";
+						out.println(getans(ans));
+						System.out.print(". ");
+					}
+				}
+			}
+		}
+		finally
+		{
+			out.close();
+		}
+	}
+	public static String getString(byte x[],int length)
+	{
+		String a="";
+		for(int i=0;i<length-1;i++)
+			a+=String.valueOf(x[i]);
+		return a;
+	}
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		if(args.length!=0&&args[0].equals("test"))
+			test();
+		else
+		{
+			PrintWriter out=new PrintWriter(("Output.txt"));
+			Scanner in=new Scanner(System.in);
+			try
+			{
+				System.out.print("Enter 1st number:");
+				int x=in.nextInt();
+				System.out.print("Enter 2nd number:");
+				int y=in.nextInt();
+				byte x1[]=binary(x);
+				byte x2[]=binary(y);
+				BoothsMultiplicationAlgorithm.multiply(x1,x2);
+				DivisionAlgorithm.divide(x1,x2);
+				String ans="Number 1: "+x+" Number 2: "+y+"\n";
+				out.println(getans(ans));
+			}
+			catch(InputMismatchException e)
+			{
+				System.out.println("Wrong input");
+			}
+			finally
+			{
+				out.close();
+			}
 
-    static {
-        UNITY = new byte[n];
-        UNITY[n-1] = 1;
-    }
+		}	
+	}
+	public static String getans(String ans)
+	{
+		ans+="Multiplication:\nBinary: ";
+		ans+=BoothsMultiplicationAlgorithm.getans();
+		ans+="\nDecimal: ";
+		ans+=String.valueOf(BoothsMultiplicationAlgorithm.ans);
+		ans+="\nDivision:\nRemainder:\nBinary: ";
+		ans+=DivisionAlgorithm.getr();
+		ans+="\nDecimal: ";
+		ans+=String.valueOf(DivisionAlgorithm.r);
+		ans+="\nQuotient:\nBinary: ";
+		ans+=DivisionAlgorithm.getq();
+		ans+="\nDecimal :";
+		ans+=String.valueOf(DivisionAlgorithm.q);
+		return ans;
+	}
 
-    public static byte[] binary(int d) {
-        int sign = d<0 ? 1 : 0;
-
-        d = Math.abs(d);
-        byte[] bin = new byte[n];
-        byte i = n;
-        i--;
-        while (d!=0) {
-            bin[i] = (byte) (d%2);
-            d = d/2;
-            i--;
-        }
-
-        if (sign==0) return bin;
-        else return twosComplement(bin);
-    }
-
-    protected static byte[] twosComplement(byte[] bin) {
-        for(byte i=0;i<Main.n;i++) {
-            if (bin[i] == 0) bin[i] = 1;
-            else bin[i] = 0;
-        }
-        add(bin, Main.UNITY);
-        return bin;
-    }
-
-    protected static void add(byte[] x, byte[] y) {
-        // y is added to x
-        // ensure equal size
-        byte carry = 0;
-        for(byte i=1;i<=x.length;i++) {
-            if (x[x.length-i]==0 && y[x.length-i]==0) {
-                x[x.length-i] = carry;
-                carry = 0;
-            }
-            else if (x[x.length-i]==1 && y[x.length-i]==1) {
-                if (carry==1) {
-                    x[x.length-i] = 1;
-                    carry = 1;
-                } else {
-                    x[x.length-i] = 0;
-                    carry = 1;
-                }
-            }
-            else {
-                if (carry==1) {
-                    x[x.length-i] = 0;
-                    carry = 1;
-                } else {
-                    x[x.length-i] = 1;
-                }
-            }
-        }
-    }
-
-    protected static void shiftRight(byte[] x) {
-        byte y = x[0];
-        byte temp;
-        for(byte i=1;i<x.length;i++) {
-            temp = x[i];
-            x[i] = y;
-            y = temp;
-        }
-    }
-
-    public static void main(String[] args) {
-	// write your code here
-       int x = -7;
-       int y = 3;
-
-       byte[] x2 = binary(x);
-       byte[] y2 = binary(y);
-
-       BoothsMultiplicationAlgorithm.multiply(x2,y2);
-    }
 }
